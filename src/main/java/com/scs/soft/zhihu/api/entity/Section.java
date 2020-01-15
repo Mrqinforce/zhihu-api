0a1bed2fead8ce39aea20a2832db1faf67e1f7cb
Mrqinforce <1293340422@qq.com>
2020-01-15 21:37:43 +0800

    Initial commit

---
+++ src/main/java/com/scs/soft/zhihu/api/entity/Section.java
@@ -0,0 +1,19 @@
+package com.scs.soft.zhihu.api.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @ClassName Section
+ * @Description TODO
+ * @Author Qin jian
+ * @Date 2020/1/15
+ * @Version 1.0
+ **/
+@Data
+@Builder
+public class Section {
+    private String sectionId;
+    private String specialId;
+    private String sectionTitle;
+}
---
+++ src/main/java/com/scs/soft/zhihu/api/mapper/SectionMapper.java
@@ -0,0 +1,24 @@
+package com.scs.soft.zhihu.api.mapper;
+
+import com.scs.soft.zhihu.api.entity.Section;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @ClassName SectionMapper
+ * @Description TODO
+ * @Author qin jian
+ * @Date 2020/1/15
+ * @Version 1.0
+ **/
+public interface SectionMapper {
+    /**
+     * 根据专题id查询标签列表
+     *
+     * @param specialId
+     * @return List<Section>
+     */
+    @Select("SELECT *FROM t_section WHERE special_id = #{specialId} ")
+    List<Section> getSectionsBySpecialId(String specialId);
+}
